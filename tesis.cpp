5accbe2f61d60ecb0cef76d671bf6f59b1e36301
ssolaric <35378033+ssolaric@users.noreply.github.com>
2017-08-28 01:39:44 -0500

    Experimentando con la segmentación

--- tesis.cpp
+++ tesis.cpp
@@ -10,11 +10,33 @@
 
 #include <opencv2/opencv.hpp>
 
+// https://github.com/opencv/opencv_attic/blob/master/opencv/samples/cpp/meanshift_segmentation.cpp
+
+using namespace cv;
+void floodFillPostprocess( Mat& img, const Scalar& colorDiff=Scalar::all(1) )
+{
+    CV_Assert( !img.empty() );
+    RNG rng = theRNG();
+    Mat mask( img.rows+2, img.cols+2, CV_8UC1, Scalar::all(0) );
+    for( int y = 0; y < img.rows; y++ )
+    {
+        for( int x = 0; x < img.cols; x++ )
+        {
+            if( mask.at<uchar>(y+1, x+1) == 0 )
+            {
+                Scalar newVal( rng(256), rng(256), rng(256) );
+                floodFill( img, mask, Point(x,y), newVal, 0, colorDiff, colorDiff );
+            }
+        }
+    }
+}
+
+
 cv::Mat quitar_margen(const cv::Mat& imagen, int margen) {
     cv::Mat nueva(imagen.rows - 2*margen, imagen.cols - 2*margen, imagen.type());
     for (int r = margen; r < imagen.rows - margen; r++) {
         for (int c = margen; c < imagen.cols - margen; c++) {
-            nueva.at<cv::Vec3b>(r-margen, c-margen) =  imagen.at<cv::Vec3b>(r, c);
+            nueva.at<cv::Vec3b>(r-margen, c-margen) = imagen.at<cv::Vec3b>(r, c);
         }
     }
     return nueva;
@@ -49,32 +71,31 @@ int main() {
         // 1. Aplicar Mean Shift a la imagen a color.
 
         cv::Mat imagen = quitar_margen(imagenes[i], 4);
-        cv::imshow(nombresImagenes[i], imagen);
-        cv::waitKey();
 
         double spatialWindowRadius = 2; // sp
         double colorWindowRadius = 5; // sr
-        // cv::pyrMeanShiftFiltering(imagen, imagen, spatialWindowRadius, colorWindowRadius);
-        // debug(imagen.rows); // height
-        // debug(imagen.cols); // width
-
-        // // 2. Aplicar Median Blur a la imagen en escala de grises.
-        // cv::cvtColor(imagen, imagen, cv::COLOR_BGR2GRAY);
-        // for (int j = 1; j < 11; j += 2) {
+        cv::pyrMeanShiftFiltering(imagen, imagen, spatialWindowRadius, colorWindowRadius);
+        floodFillPostprocess(imagen, Scalar::all(2));
+        // 2. Aplicar Median Blur a la imagen en escala de grises.
+        //cv::cvtColor(imagen, imagen, cv::COLOR_BGR2GRAY);
+        // for (int j = 1; j < 5; j += 2) {
         //     cv::medianBlur(imagen, imagen, j);
         // }
 
-        // // 3. Hacer un resize
+        // 3. Hacer un resize
         // cv::resize(imagen, imagen, cv::Size(0, 0), 8.0, 8.0, cv::INTER_CUBIC);
 
-        // // 4. Aumentar el contraste
-        // cv::equalizeHist(imagen, imagen);
+        // 4. Aumentar el contraste
+        //cv::equalizeHist(imagen, imagen);
 
         // 5. Hacer el threshold
-        // cv::inRange(imagen, cv::Scalar(0), cv::Scalar(20), imagen);
+        // cv::inRange(imagen, cv::Scalar(0), cv::Scalar(10), imagen);
 
-        // cv::imshow(nombresImagenes[i], imagen);
-        // cv::waitKey();
+        cv::namedWindow(nombresImagenes[i], cv::WINDOW_AUTOSIZE);
+        cv::imshow(nombresImagenes[i], imagen);
+        cv::waitKey(2000);
         // cv::waitKey(5000);
     }
 }
+
+// TODO: preguntar cómo hacer la segmentación de las cabezas y preparar la normalización de pose
