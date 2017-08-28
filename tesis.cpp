2aae9c5305146a3e5f229230dc7453118815d3f5
ssolaric <35378033+ssolaric@users.noreply.github.com>
2017-08-28 13:50:07 -0500

    Modular código

--- tesis.cpp
+++ tesis.cpp
@@ -67,12 +67,9 @@ cv::Mat quitar_margen(const cv::Mat& imagen, int margen) {
     return nueva;
 }
 
-int main() {
+void leer_imagenes(std::vector<std::string>& nombresImagenes, std::vector<cv::Mat>& imagenes) {
     struct dirent** namelist;
     int n = scandir("./Imagenes", &namelist, NULL, versionsort);
-    std::vector<std::string> nombresImagenes;
-    std::vector<cv::Mat> imagenes;
-    
     if (n < 0) {
         perror("scandir");
     }
@@ -90,42 +87,55 @@ int main() {
         }
         free(namelist);
     }
+}
+
+void procesar_imagen(cv::Mat& imagen, const std::string& nombreImagen) {
+    imagen = equalizeIntensity(imagen); // ecualización del canal de intensidad 
+    double spatialWindowRadius = 2; // sp
+    double colorWindowRadius = 5; // sr
+    cv::pyrMeanShiftFiltering(imagen, imagen, spatialWindowRadius, colorWindowRadius);
+    //floodFillPostprocess(imagen, Scalar::all(2));
+    // 2. Aplicar Median Blur a la imagen en escala de grises.
+    cv::cvtColor(imagen, imagen, cv::COLOR_BGR2GRAY);
+    for (int j = 1; j < 11; j += 2) {
+        // cv::medianBlur(imagen, imagen, j);
+        cv::GaussianBlur(imagen, imagen, Size(j, j), 0);
+    }
+    
+    // 3. Hacer un resize
+    // ¿Conviene o no conviene?
+    // Conviene: si el resize es bueno, permite mayor detalle para la reconstrucción.
+    // No conviene: ya no cogería la forma original del espermatozoide.
+    cv::resize(imagen, imagen, cv::Size(0, 0), 8.0, 8.0, cv::INTER_CUBIC);
+
+    // 4. Aumentar el contraste
+    cv::equalizeHist(imagen, imagen);
+    // 5. Hacer el threshold
+
+    // El área oscura dentro de un espermatozoide (los píxeles entre 0 y 2) es la parte inferior de la cabeza (cercana a la cola).
+    // No funciona bien con la imagen 14 (posible opción: descartarla)
+    cv::inRange(imagen, cv::Scalar(3), cv::Scalar(25), imagen);
+    
+    // cv::adaptiveThreshold(imagen, imagen, 255, ADAPTIVE_THRESH_MEAN_C,  THRESH_BINARY_INV, 3, 3);
+    // cv::threshold(imagen, imagen, 40, 255, THRESH_BINARY);
+    std::string ruta = std::string("./ImagenesProcesadas/") + nombreImagen;
+    cv::imwrite(ruta, imagen);
+}
+
+
+// Según Francisco, las imágenes que no tienen out son luego de aplicar su clasificador. Es decir, trabajaré con estas (del 1 al 100).
+
+int main() {
+    std::vector<std::string> nombresImagenes;
+    std::vector<cv::Mat> imagenes;
+    leer_imagenes(nombresImagenes, imagenes);
 
     // for (size_t i = 0; i < imagenes.size(); i++) {
     for (size_t i = 0; i < imagenes.size(); i++) {
         // 1. Aplicar Mean Shift a la imagen a color.
 
         cv::Mat imagen = quitar_margen(imagenes[i], 4);
-        imagen = equalizeIntensity(imagen);
-        double spatialWindowRadius = 2; // sp
-        double colorWindowRadius = 5; // sr
-        cv::pyrMeanShiftFiltering(imagen, imagen, spatialWindowRadius, colorWindowRadius);
-        //floodFillPostprocess(imagen, Scalar::all(2));
-        // 2. Aplicar Median Blur a la imagen en escala de grises.
-        cv::cvtColor(imagen, imagen, cv::COLOR_BGR2GRAY);
-        for (int j = 1; j < 11; j += 2) {
-            cv::medianBlur(imagen, imagen, j);
-            // cv::GaussianBlur(imagen, imagen, Size(j, j), 0);
-        }
-        
-        // 3. Hacer un resize
-        // ¿Conviene o no conviene?
-        // Conviene: si el resize es bueno, permite mayor detalle para la reconstrucción.
-        // No conviene: ya no cogería la forma original del espermatozoide.
-        // cv::resize(imagen, imagen, cv::Size(0, 0), 8.0, 8.0, cv::INTER_CUBIC);
-
-        // 4. Aumentar el contraste
-        cv::equalizeHist(imagen, imagen);
-        // 5. Hacer el threshold
-
-        // El área oscura dentro de un espermatozoide (los píxeles entre 0 y 2) es la parte inferior de la cabeza (cercana a la cola).
-
-        cv::inRange(imagen, cv::Scalar(3), cv::Scalar(25), imagen);
-        // cv::adaptiveThreshold(imagen, imagen, 255, ADAPTIVE_THRESH_MEAN_C,  THRESH_BINARY_INV, 3, 3);
-        // cv::threshold(imagen, imagen, 40, 255, THRESH_BINARY);
-        std::string ruta = std::string("./ImagenesProcesadas/") + nombresImagenes[i];
-        cv::imwrite(ruta, imagen);
-
+        procesar_imagen(imagen, nombresImagenes[i]);
         // cv::namedWindow(nombresImagenes[i], cv::WINDOW_AUTOSIZE);
         // cv::imshow(nombresImagenes[i], imagen);
         // cv::waitKey(2000);
